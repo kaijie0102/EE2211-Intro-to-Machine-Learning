8cbdd65b1e26d8f2e240c08de5e288d919039b4d
kai jie <leowkaijie@gmail.com>
2024-05-02 22:21:54 +0800

    Add signum classification

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -16,21 +16,23 @@ README
 """ Section A: Variables """
 # ================================================
 # for training
-x = np.array([1,0,1, 1,-1,1]).reshape(2,3)
+x = np.array([4,7,10,2,3,9]).reshape(6,1)
 """ 
 there is no need for 1 to be in first column of X, 
 fit_transform function will do it
 """
-y = np.array([0,1]).reshape(2,1)
+y = np.array([-1,-1,-1,1,1,1]).reshape(6,1)
 
 # for testing
-x_test = np.array([]).reshape
+x_test = np.array([6]).reshape(1,1)
+
 # ================================================
 
 # Generate polynomial feature, P from X
-order = 3
+order = 4
 poly = PolynomialFeatures(order)
 P = poly.fit_transform(x) # transforming x into p
+P_test = poly.fit_transform(x_test)
 print("Matrix P: \n", P)
 
 """ Section B: Non ridge Regression. If using this section, comment out Section C """
@@ -38,25 +40,34 @@ print("Matrix P: \n", P)
 if (P.shape[1] > P.shape[0]):
     # Underdetermined System m < d
     w_dual = P.T @ inv(P @ P.T) @ y
-    print(w_dual)
+    print("w_dual: ", w_dual)
+    w = w_dual
 else: 
     # Overdetermined System m > d
     w_primal = inv(P.T @ P) @ P.T @ y
-    print(w_primal)
+    print("w_primal: ", w_primal)
+    w = w_primal
 # ================================================
 
 """ Section C: Ridge Regression. If using this section, comment out Section B """
 # ================================================
 # Underdetermined System m < d
-if (P.shape[1] > P.shape[0]):
-    reg_L = 0.0001*np.identity(P.shape[0]) # size of P.T @ P = m x m (rows)
-    w_dual_ridge = P.T @ inv(P @ P.T + reg_L) @ y
-    print(w_dual_ridge)
-else:
-    # Overdetermined System m > d
-    reg_L = 0.0001*np.identity(P.shape[1]) # size of P.T @ P = d x d (columns)
-    w_primal_ridge = inv(P.T @ P + reg_L) @ P.T @ y
-    print(w_primal_ridge)
+# if (P.shape[1] > P.shape[0]):
+#     reg_L = 0.0001*np.identity(P.shape[0]) # size of P.T @ P = m x m (rows)
+#     w_dual_ridge = P.T @ inv(P @ P.T + reg_L) @ y
+#     print(w_dual_ridge)
+# else:
+#     # Overdetermined System m > d
+#     reg_L = 0.0001*np.identity(P.shape[1]) # size of P.T @ P = d x d (columns)
+#     w_primal_ridge = inv(P.T @ P + reg_L) @ P.T @ y
+#     print(w_primal_ridge)
 # ================================================
+
+# best training error count
+y_calculated = P_test @ w
+print("y_calculated: ", y_calculated)
+
+y_class_predicted = np.sign(y_calculated)
+print(y_class_predicted)
 
 
